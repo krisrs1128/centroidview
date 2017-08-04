b9a7b8c7fa1aa25d3db265e3963fa5d946e47ce5
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-04 11:51:19 -0400

    preparing data for visualization

--- R/centroidview.R
+++ R/centroidview.R
@@ -7,10 +7,18 @@
 #' @importFrom tibble data_frame as_data_frame
 #' @importFrom dendextend as.phylo.dendrogram get_nodes_xy
 #' @importFrom ape as.phylo
+#' @importFrom plyr dlply .
 #'
 #' @export
-centroidview <- function(x, tree, groups, samples, width = NULL, height = NULL,
+centroidview <- function(x,
+                         tree,
+                         group,
+                         facet,
+                         facet_x,
+                         width = NULL,
+                         height = NULL,
                          elementId = NULL) {
+
   ## prepare the tree coordinates data
   dendro <- reorder(as.dendrogram(tree), -colMeans(x))
   phy <- as.phylo(dendro)
@@ -19,23 +27,49 @@ centroidview <- function(x, tree, groups, samples, width = NULL, height = NULL,
     as_data_frame() %>%
     rename(y = V1, x = V2) %>%
     mutate(x = -x)
-  node_data$id <- seq_along(node_data$y)
+  node_data$column <- node_data$y %>%
+    seq_along() %>%
+    as.character()
 
   phy_df <- as_data_frame(phy$edge) %>%
-    rename(parent = V1, id = V2) %>%
+    rename(parent = V1, column = V2) %>%
+    mutate_all(as.character) %>%
     left_join(node_data)
-  mapping <- setNames(phy$tip, seq_along(phy$tip))
-  phy_df <- rbind(
-    phy_df,
-    data_frame(parent = "", id = phy$edge[1, 1], y = 0.03, x = mean(phy_df$x))
-  )
 
-  # forward options using x
+  tmp_root <- data_frame(
+    parent = "",
+    column = as.character(phy$edge[1, 1]), y = 0.03,
+    x = mean(phy_df$x)
+  )
+  phy_df <- rbind(phy_df, tmp_root)
+
+  ## prepare the melted value data
+  mx <- data.frame("row" = seq_len(nrow(x)), "x" = x) %>%
+    gather(column, value, -row) %>%
+    mutate(
+      column = gsub("x\\.", "", column),
+      group = group[column],
+      facet = facet[row],
+      facet_x = facet_x[row]
+    ) %>%
+    as_data_frame()
+
+  melted_data <- mx %>%
+    left_join(phy_df %>% select(-y, -parent))
+
+  ## construct the ts_data
+  ts_data <- melted_data %>%
+    dlply(.(facet, column, group), identity)
+  names(ts_data) <- NULL
+
+  ## forward options using x
   fw = list(
-    "phy_df" = phy_df
+    "phy_df" = phy_df,
+    "melted_data" = melted_data,
+    "ts_data" = ts_data
   )
 
-  # create widget
+  ## create widget
   htmlwidgets::createWidget(
     name = 'centroidview',
     fw,
