9193721aa21b9452f2b62c9e98f84cdb924fc56e
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-04 09:58:33 -0400

    htmlwidgets scaffolding

---
+++ R/centroidview.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+centroidview <- function(message, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'centroidview',
+    x,
+    width = width,
+    height = height,
+    package = 'centroidview',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for centroidview
+#'
+#' Output and render functions for using centroidview within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a centroidview
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name centroidview-shiny
+#'
+#' @export
+centroidviewOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'centroidview', width, height, package = 'centroidview')
+}
+
+#' @rdname centroidview-shiny
+#' @export
+renderCentroidview <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, centroidviewOutput, env, quoted = TRUE)
+}
