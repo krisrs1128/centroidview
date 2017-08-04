76d498015e6d202bfb358e3b3962b6044717160d
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-04 11:10:25 -0400

    forwarding the phylogenetic data frame

--- R/centroidview.R
+++ R/centroidview.R
@@ -1,52 +1,47 @@
-#' <Add Title>
+#' Study Centroids from a Hierarchical Clustering
 #'
-#' <Add Description>
+#' This creates an interactive heatmap + hierarchical clustering view.
 #'
 #' @import htmlwidgets
+#' @importFrom dplyr rename mutate
+#' @importFrom tibble data_frame as_data_frame
+#' @importFrom dendextend as.phylo.dendrogram get_nodes_xy
+#' @importFrom ape as.phylo
 #'
 #' @export
-centroidview <- function(message, width = NULL, height = NULL, elementId = NULL) {
+centroidview <- function(x, tree, groups, samples, width = NULL, height = NULL,
+                         elementId = NULL) {
+  ## prepare the tree coordinates data
+  dendro <- reorder(as.dendrogram(tree), -colMeans(x))
+  phy <- as.phylo(dendro)
+
+  node_data <- get_nodes_xy(dendro) %>%
+    as_data_frame() %>%
+    rename(y = V1, x = V2) %>%
+    mutate(x = -x)
+  node_data$id <- seq_along(node_data$y)
+
+  phy_df <- as_data_frame(phy$edge) %>%
+    rename(parent = V1, id = V2) %>%
+    left_join(node_data)
+  mapping <- setNames(phy$tip, seq_along(phy$tip))
+  phy_df <- rbind(
+    phy_df,
+    data_frame(parent = "", id = phy$edge[1, 1], y = 0.03, x = mean(phy_df$x))
+  )
 
   # forward options using x
-  x = list(
-    message = message
+  fw = list(
+    "phy_df" = phy_df
   )
 
   # create widget
   htmlwidgets::createWidget(
     name = 'centroidview',
-    x,
+    fw,
     width = width,
     height = height,
     package = 'centroidview',
     elementId = elementId
   )
-}
-
-#' Shiny bindings for centroidview
-#'
-#' Output and render functions for using centroidview within Shiny
-#' applications and interactive Rmd documents.
-#'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a centroidview
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
-#' @name centroidview-shiny
-#'
-#' @export
-centroidviewOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'centroidview', width, height, package = 'centroidview')
-}
-
-#' @rdname centroidview-shiny
-#' @export
-renderCentroidview <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, centroidviewOutput, env, quoted = TRUE)
 }
