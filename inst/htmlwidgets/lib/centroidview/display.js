a5d2dfa5f50ed739acbe68c497f30dab7dad1a04
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-04 16:19:09 -0400

    drawing the tree
    it looks funky, but there is something here

--- inst/htmlwidgets/lib/centroidview/display.js
+++ inst/htmlwidgets/lib/centroidview/display.js
@@ -5,17 +5,60 @@
  * date: 08/04/2017
  */
 
-function centroidview(elem, tree) {
+function centroidview(elem, tree, data) {
   var param = parameter_defaults({});
-  tree = HTMLWidgets.dataframeToD3(tree);
-  var display_opts = display_defaults(tree);
+  var display = display_defaults(tree);
+
+  // setup background display elements
   setup(elem, param);
+  add_groups(elem, param);
+  param.elem_width = param.elem_width - param.margin.right - param.margin.left;
+  param.elem_height = param.elem_height - param.margin.top - param.margin.bottom;
+
+  // get some of the scales
+  var scales = scales_dictionary(tree, data, param);
+  var facet_x = extract_unique(data, "facet_x");
+
+  // Draw the tree
+  draw_tree(elem, display.root, scales.tree_x, scales.tree_y);
+
+}
+
+function draw_tree(elem, root, tree_x_scale, tree_y_scale) {
+  d3.cluster()(root);
+  d3.select(elem)
+    .select("#subtree_0")
+    .selectAll(".hcnode")
+    .data(root.descendants(), id_fun).enter()
+    .append("circle")
+    .attrs({
+      "class": "hcnode",
+      "r": 2,
+      "fill": "#555",
+      "fill-opacity": 0.4,
+      "cx": function(d) { return tree_x_scale(d.data.y); },
+      "cy": function(d) { return tree_y_scale(d.data.x); }
+    });
+
+  var link_fun = d3.linkHorizontal()
+      .x(function(d) { return tree_x_scale(d.data.y); })
+      .y(function(d) { return tree_y_scale(d.data.x); });
+
+  d3.select(elem)
+    .select("#links")
+    .selectAll(".link")
+    .data(root.links()).enter()
+    .append("path")
+    .attrs({
+      "class": "link",
+      "stroke": "#555",
+      "d": link_fun
+    });
 }
 
 function setup(elem, param) {
   // create margins using margin convention
   // https://bl.ocks.org/mbostock/3019563
-  var margin = {top: 20, right: 10, bottom: 20, left: 10};
   var svg_elem = d3.select(elem)
       .append("svg")
       .attrs({
@@ -28,7 +71,33 @@ function setup(elem, param) {
       "fill": "#F8F8F8",
       "width": param.elem_width,
       "height": param.elem_height
-    })
+    });
+
+  svg_elem.append("g")
+    .attrs({
+      "transform": "translate(" + param.margin.left + "," + param.margin.top + ")",
+      "id": "base"
+    });
+}
+
+function add_groups(elem, param) {
+  // layer 0 represents all samples / features, and is always on top (even if
+  // invisible)
+  var group_labels = ["tiles", "tile_cover", "links", "voronoi", "group_histo",
+                      "histo_axis"];
+  for (var k = param.n_clusters; k >= 0; k--) {
+    group_labels = group_labels.concat([
+      "subtree_" + k,
+      "hm_focus_" + k,
+      "time_series_" + k,
+      "centroids_" + k
+    ]);
+  }
+
+  d3.select(elem)
+    .select("#base")
+    .selectAll("g")
+    .data(group_labels).enter()
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("id", function(d) { return d; });
 }
